efde7c5c78c0365ab8490a798e05e223aac203b8
Gonzalo Diaz <info@ventra.com.co>
2018-07-02 21:28:41 +0100

    finished landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'conocenos', to: 'pages#conocenos', as: :conocenos
+  get 'productos', to: 'pages#productos', as: :productos
+  get 'servicios', to: 'pages#servicios', as: :servicios
+  get 'contacto', to: 'pages#contacto', as: :contacto
+
+
+
   root to: 'pages#home'
 end
